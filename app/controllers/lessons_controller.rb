29e511eea26b1f8fc0266d5cb124a0bec8af5f86
Mick Grom <mickgrom@gmail.com>
2018-06-12 14:06:49 +0200

    saved the show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,4 +8,24 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
   end
 
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @user = current_user
+    @lesson = Lesson.new(lesson_params)
+    @lesson.user = @user
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:cuisine, :location, :title, :description, :image, :capacity)
+  end
 end
