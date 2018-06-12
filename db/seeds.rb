d95d54de42696f816d41567e39ace767a38fbbcd
Mick Grom <mickgrom@gmail.com>
2018-06-12 15:30:38 +0200

    added pictures to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
 
@@ -18,14 +15,26 @@ puts 'Creating 5 fake USERS...'
     first_name: Faker::Artist.name,
     last_name:  Faker::Name.last_name,
     email:      Faker::Internet.email,
-    password:   Faker::Internet.password,
-    image:      'http://apod.nasa.gov/apod/image/1407/m31_bers_960.jpg'
+    password:   Faker::Internet.password
   )
   user.save!
 end
 puts 'Finished Users!'
 
 p
+
+images = [
+  'http://apod.nasa.gov/apod/image/1407/m31_bers_960.jpg',
+  'https://images.pexels.com/photos/375889/pexels-photo-375889.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/298926/woman-kitchen-man-everyday-life-298926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/6245/kitchen-cooking-interior-decor.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/106877/pexels-photo-106877.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/33242/cooking-ingredient-cuisine-kitchen.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/375468/pexels-photo-375468.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/271458/pexels-photo-271458.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/66639/pexels-photo-66639.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/8572/food-chicken-meat-outdoors.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+]
 
 puts 'Creating 5 fake LESSONS...'
 5.times do
@@ -35,7 +44,8 @@ puts 'Creating 5 fake LESSONS...'
     title:      Faker::Book.title,
     user:       User.all.sample,
     description:Faker::GameOfThrones.quote,
-    capacity:   2
+    capacity:   [1..10].sample,
+    image:      images.sample
   )
   lesson.save!
 end
