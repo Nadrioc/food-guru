f21b4aa70ef0796780743ef836aad5254817d63d
Mick Grom <mickgrom@gmail.com>
2018-06-12 13:23:51 +0200

    show page incl css, updated the seed file

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,5 @@
 class LessonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @lessons = Lesson.all
   end
