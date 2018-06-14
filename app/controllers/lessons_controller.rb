15e1d8d1226b3eec89e3bfd7a145c282b3be7869
Mick Grom <mickgrom@gmail.com>
2018-06-14 16:12:58 +0200

    added google maps cards to click on + google maps on show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -17,14 +17,22 @@ class LessonsController < ApplicationController
     @markers = @lessons.where.not(latitude: nil, longitude: nil).map do |lesson|
       {
         lat: lesson.latitude,
-        lng: lesson.longitude#,
-        # infoWindow: { content: render_to_string(partial: "/lessons/map_box", locals: { lesson: lesson }) }
+        lng: lesson.longitude,
+        infoWindow: { content: render_to_string(partial: "/lessons/map_box", locals: { lesson: lesson }) }
       }
     end
   end
 
   def show
     @lesson = Lesson.find(params[:id])
+
+
+    @markers = [
+      {
+        lat: @lesson.latitude,
+        lng: @lesson.longitude
+      }
+    ]
   end
 
   def new
