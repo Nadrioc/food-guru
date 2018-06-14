bf45eb9835b5734c0a0f4495498268ca9cee7708
Samantha Kopec <skopec25@gmail.com>
2018-06-14 11:53:58 +0200

    creates booking form and adding lessons to user profile

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  before_action :set_lesson
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @lesson.user = @chef
+    @student = current_user
+    @booking = Booking.new
+    @booking.status = "Pending"
+    redirect_to lesson_path(@lesson)
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:lesson_id])
+  end
 end
