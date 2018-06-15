59c3dba58e829214c555f1f6c7e83ee9fc61a047
Laura Green <lauraduv@gmail.com>
2018-06-15 11:52:25 +0200

    Profile and Dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @listed_lessons = @user.listed_lessons
+    @listed_lessons = @user.listed_lessons.first(3)
     @bookings = Booking.where(user: @user)
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  # private
 
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
-    # Will raise ActiveModel::ForbiddenAttributesError
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :image,)
-  end
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :description, :image,)
+  # end
 end
 
 
